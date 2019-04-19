f2d812f4df2aa5b3943bef8c40eb7fe3feb1eead
sologuboved <sologuboved@gmail.com>
2019-04-19 22:07:28 +0300

    added pid operations

--- bot.py
+++ bot.py
@@ -1,16 +1,8 @@
-import os
-import sys
 import datetime
 from telegram.ext import Updater
 from tkn import TOKEN
 from get_random_passage import *
-
-
-def write_pid():
-    pid_fname = '{}_{}.pid'.format(os.path.splitext(os.path.basename(sys.argv[0]))[0], str(os.getpid()))
-    with open(pid_fname, 'w') as handler:
-        handler.write(str())
-    return pid_fname
+from pid_operations import write_pid
 
 
 def random_quoter(bot, job):
@@ -27,10 +19,13 @@ def get_message(extractor):
     return title + '\n\n' + passage
 
 
-if __name__ == '__main__':
-    write_pid()
+def main():
     updater = Updater(token=TOKEN)
     job_queue = updater.job_queue
-    job_queue.run_daily(random_quoter, time=datetime.time(17, 00))
-    # job_queue.run_repeating(another_func, datetime.timedelta(hours=3.0), first=0)
+    job_queue.run_daily(random_quoter, time=datetime.time(22, 10))
     updater.start_polling()
+
+
+if __name__ == '__main__':
+    write_pid()
+    main()
